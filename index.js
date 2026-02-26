75eed18162bccf5fe3a214df1d2836018f037736
untalanca5-coder <untalanca5@gmail.com>
2026-02-26 11:34:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ window.addEventListener('load', () => {
 function getUser() {
   let html = '';
   //fetch api
-  fetch('https://hamboorgir.onrender.com/api/users',{mode: 'cors'})
+  fetch('https://api.sampleapis.com/beers/ale',{mode: 'cors'})
   .then(response=>{console.log(response); return response.json();
   })
   .then(data=>{
     console.log(data);
     data.forEach(element=>{
-      html+=`<li>${element.first_name} ${element.last_name}</li>`;
+      html+=`<li>${element.price} ${element.name}${element.image</li>`;
     })
     content.innerHTML = html;
   })
   .catch(err=>console.log(err));
 }
+
 
     
