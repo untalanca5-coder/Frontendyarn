0184210cd193513d0a82dd6a7e38836685dc4f3c
untalanca5-coder <untalanca5@gmail.com>
2026-03-06 12:28:10 +0800

    Refactor item submission and user retrieval logic

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-const content = document.querySelector('#content');
-window.addEventListener('load', () => {
-  getUser();
-})
+const content = document.querySelector("#content");
+const submit = document.querySelector("#add");
 
-function getUser() {
-  let html = '';
-  //fetch api
-  fetch('https://api.sampleapis.com/beers/ale',{mode: 'cors'})
-  .then(response=>{console.log(response); return response.json();
-  })
-  .then(data=>{
-    console.log(data);
-    data.forEach(element=>{
-      html+=`<li><img= src={element.image} style ="width=50px" >
-      ${element.price} ${element.name}</li>`;
+submit.addEventListener("click", () => {
+
+    let itemName = document.querySelector("#item-name").value;
+    let unitPrice = document.querySelector("#price-unit").value;
+    let quantity = document.querySelector("#quantity").value;
+    let supplier = document.querySelector("#supplier").value;
+    let formData = {itemName, unitPrice, quantity, supplier};
+
+    fetch("https://pcs112-products.onrender.com/api/users",{
+        method: "POST",
+        body: JSON.stringify(formData),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).catch((error) => {
+        console.log(error);
     })
-    content.innerHTML = html;
-  })
-  .catch(err=>console.log(err));
-}
+    alert("Item added successfully!");
+    location.reload();
+});
 
+    window.addEventListener("load", () => {
+        getUsers();
+    });
 
-    
+    function getUsers() {
+        let html = "";
 
+        fetch("https://pcs112-products.onrender.com/api/users", {mode: 'cors'})
+        .then(response => {
+            console.log(response);
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            data.forEach(element => {
+                html += `<li>${element.id} ${element.itemName} ${element.unitPrice} ${element.quantity} ${element.supplier}</li>`;
+            })
 
+            content.innerHTML = html;
+        })
+    }
